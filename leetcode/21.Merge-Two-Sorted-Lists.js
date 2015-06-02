e9ec84163bd1a0050142746ee46c30e87812d414
Star Xu <873111137@qq.com>
2015-06-02 18:33:39 +0800

    Sorted list

---
+++ leetcode/21.Merge-Two-Sorted-Lists.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    var head = l1.val<l2.val?l1:l2;
+    var p = new ListNode();
+    while(l1 || l2){
+    	if(l1 && l2){
+    		if(l1.val<l2.val){
+    			p.next = l1;
+    			p = p.next;
+    			l1 = l1.next;
+    		}else{
+    			p.next = l2;
+    			p = p.next;
+    			l2 = l2.next;
+    		}
+    	}else if(l1==null){
+    		p.next = l2;
+    		return head;
+    	}else{
+    		p.next = l1;
+    		return head;
+    	}
+    }
+};
